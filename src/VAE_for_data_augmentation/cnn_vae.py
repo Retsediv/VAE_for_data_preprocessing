bd89598eba2b7dc4f9c59ddd8d84a9531a36b742
retsediv <retsediv1@gmail.com>
2019-01-07 22:43:20 +0200

    Refactoring

--- src/VAE_for_data_augmentation/cnn_vae.py
+++ src/VAE_for_data_augmentation/cnn_vae.py
@@ -29,75 +29,16 @@ class VAE(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
     def encode(self, x: Variable) -> (Variable, Variable):
-        """Input vector x -> fully connected 1 -> ReLU -> (fully connected
-        21, fully connected 22)
-
-        Parameters
-        ----------
-        x : [128, 784] matrix; 128 digits of 28x28 pixels each
-
-        Returns
-        -------
-
-        (mu, logvar) : ZDIMS mean units one for each latent dimension, ZDIMS
-            variance units one for each latent dimension
-
-        """
-
         # h1 is [128, 400]
-        h1 = self.relu(self.fc1(x))  # type: Variable
+        h1 = self.relu(self.fc1(x))
         return self.fc21(h1), self.fc22(h1)
 
     def reparameterize(self, mu: Variable, logvar: Variable) -> Variable:
-        """THE REPARAMETERIZATION IDEA:
-
-        For each training sample (we get 128 batched at a time)
-
-        - take the current learned mu, stddev for each of the ZDIMS
-          dimensions and draw a random sample from that distribution
-        - the whole network is trained so that these randomly drawn
-          samples decode to output that looks like the input
-        - which will mean that the std, mu will be learned
-          *distributions* that correctly encode the inputs
-        - due to the additional KLD term (see loss_function() below)
-          the distribution will tend to unit Gaussians
-
-        Parameters
-        ----------
-        mu : [128, ZDIMS] mean matrix
-        logvar : [128, ZDIMS] variance matrix
-
-        Returns
-        -------
-
-        During training random sample from the learned ZDIMS-dimensional
-        normal distribution; during inference its mean.
-
-        """
-
         if self.training:
-            # multiply log variance with 0.5, then in-place exponent
-            # yielding the standard deviation
             std = logvar.mul(0.5).exp_()  # type: Variable
-            # - std.data is the [128,ZDIMS] tensor that is wrapped by std
-            # - so eps is [128,ZDIMS] with all elements drawn from a mean 0
-            #   and stddev 1 normal distribution that is 128 samples
-            #   of random ZDIMS-float vectors
             eps = Variable(std.data.new(std.size()).normal_())
-            # - sample from a normal distribution with standard
-            #   deviation = std and mean = mu by multiplying mean 0
-            #   stddev 1 sample with desired std and mu, see
-            #   https://stats.stackexchange.com/a/16338
-            # - so we have 128 sets (the batch) of random ZDIMS-float
-            #   vectors sampled from normal distribution with learned
-            #   std and mu for the current input
             return eps.mul(std).add_(mu)
-
         else:
-            # During inference, we simply spit out the mean of the
-            # learned distribution for the current input.  We could
-            # use a random sample from the distribution, but mu of
-            # course has the highest probability.
             return mu
 
     def decode(self, z: Variable) -> Variable:
@@ -119,22 +60,9 @@ class VAE(nn.Module):
 
 
 def loss_function(recon_x, x, mu, logvar, BATCH_SIZE) -> Variable:
-    # how well do input x and output recon_x agree?
     BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784))
 
-    # KLD is Kullback–Leibler divergence -- how much does one learned
-    # distribution deviate from another, in this specific case the
-    # learned distribution from the unit Gaussian
-
-    # see Appendix B from VAE paper:
-    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-    # https://arxiv.org/abs/1312.6114
-    # - D_{KL} = 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    # note the negative D_{KL} in appendix B of the paper
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-    # Normalise by same number of elements as in reconstruction
     KLD /= BATCH_SIZE * 784
 
-    # BCE tries to make our reconstruction as accurate as possible
-    # KLD tries to push the distributions as close as possible to unit Gaussian
     return BCE + KLD
--- src/VAE_for_noise_reduction/cnn_vae.py
+++ src/VAE_for_noise_reduction/cnn_vae.py
@@ -15,7 +15,6 @@ class VAE(nn.Module):
         # 28 x 28 pixels = 784 input pixels, 400 outputs
         self.fc1 = nn.Linear(784, 400)
         # rectified linear unit layer from 400 to 400
-        # max(0, x)
         self.relu = nn.ReLU()
         self.fc21 = nn.Linear(400, ZDIMS)  # mu layer
         self.fc22 = nn.Linear(400, ZDIMS)  # logvariance layer
@@ -29,75 +28,16 @@ class VAE(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
     def encode(self, x: Variable) -> (Variable, Variable):
-        """Input vector x -> fully connected 1 -> ReLU -> (fully connected
-        21, fully connected 22)
-
-        Parameters
-        ----------
-        x : [128, 784] matrix; 128 digits of 28x28 pixels each
-
-        Returns
-        -------
-
-        (mu, logvar) : ZDIMS mean units one for each latent dimension, ZDIMS
-            variance units one for each latent dimension
-
-        """
-
-        # h1 is [128, 400]
         h1 = self.relu(self.fc1(x))  # type: Variable
         return self.fc21(h1), self.fc22(h1)
 
     def reparameterize(self, mu: Variable, logvar: Variable) -> Variable:
-        """THE REPARAMETERIZATION IDEA:
-
-        For each training sample (we get 128 batched at a time)
-
-        - take the current learned mu, stddev for each of the ZDIMS
-          dimensions and draw a random sample from that distribution
-        - the whole network is trained so that these randomly drawn
-          samples decode to output that looks like the input
-        - which will mean that the std, mu will be learned
-          *distributions* that correctly encode the inputs
-        - due to the additional KLD term (see loss_function() below)
-          the distribution will tend to unit Gaussians
-
-        Parameters
-        ----------
-        mu : [128, ZDIMS] mean matrix
-        logvar : [128, ZDIMS] variance matrix
-
-        Returns
-        -------
-
-        During training random sample from the learned ZDIMS-dimensional
-        normal distribution; during inference its mean.
-
-        """
-
         if self.training:
-            # multiply log variance with 0.5, then in-place exponent
-            # yielding the standard deviation
             std = logvar.mul(0.5).exp_()  # type: Variable
-            # - std.data is the [128,ZDIMS] tensor that is wrapped by std
-            # - so eps is [128,ZDIMS] with all elements drawn from a mean 0
-            #   and stddev 1 normal distribution that is 128 samples
-            #   of random ZDIMS-float vectors
             eps = Variable(std.data.new(std.size()).normal_())
-            # - sample from a normal distribution with standard
-            #   deviation = std and mean = mu by multiplying mean 0
-            #   stddev 1 sample with desired std and mu, see
-            #   https://stats.stackexchange.com/a/16338
-            # - so we have 128 sets (the batch) of random ZDIMS-float
-            #   vectors sampled from normal distribution with learned
-            #   std and mu for the current input
             return eps.mul(std).add_(mu)
 
         else:
-            # During inference, we simply spit out the mean of the
-            # learned distribution for the current input.  We could
-            # use a random sample from the distribution, but mu of
-            # course has the highest probability.
             return mu
 
     def decode(self, z: Variable) -> Variable:
@@ -120,27 +60,11 @@ class VAE(nn.Module):
 
 
 def loss_function(recon_x, x, mu, logvar, BATCH_SIZE) -> Variable:
-    # how well do input x and output recon_x agree?
     x = x.view(-1, 784)
     
-#     print("recon_x: ", recon_x.shape)
-#     print("x: ", x.shape)
-
     BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784))
 
-    # KLD is Kullback–Leibler divergence -- how much does one learned
-    # distribution deviate from another, in this specific case the
-    # learned distribution from the unit Gaussian
-
-    # see Appendix B from VAE paper:
-    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-    # https://arxiv.org/abs/1312.6114
-    # - D_{KL} = 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    # note the negative D_{KL} in appendix B of the paper
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-    # Normalise by same number of elements as in reconstruction
     KLD /= BATCH_SIZE * 784
 
-    # BCE tries to make our reconstruction as accurate as possible
-    # KLD tries to push the distributions as close as possible to unit Gaussian
     return BCE + KLD
